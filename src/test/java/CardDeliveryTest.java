7b0f7b27b1eef9cc157ba1343f90e00ffe17d05a
Ferrk <qeri4gyw@mail.ru>
2022-09-01 01:43:10 +0300

    add file

---
+++ src/test/java/CardDeliveryTest.java
@@ -0,0 +1,28 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class CardDeliveryTest {
+    @Test
+    public void shouldSuccessfulFormSubmission() {
+        open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue("Тула");
+        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.DELETE);
+        String verificationDate = LocalDate.now().plusDays(4)           //Текущая дата плюс 4 дня
+                .format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));     //Формат даты день.месяц.год
+        $("[data-test-id=date] input").setValue(verificationDate);
+        $("[data-test-id=name] input").setValue("Артем Анов");
+        $("[data-test-id=phone] input").setValue("+79012345678");
+        $("[data-test-id=agreement]").click();
+        $(".button").shouldHave(Condition.text("Забронировать")).click();
+        $("[data-test-id=notification]")
+                .shouldHave(Condition.text("Успешно! Встреча успешно забронирована на " + verificationDate),
+                        Duration.ofSeconds(15));                        //Загрузка не более 15 секунд
+    }
+}
